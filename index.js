1c4795babb3feab7513dd457f64e7020a06bc035
Marcelo Nogai <yuassa11@gmail.com>
2024-02-20 17:05:56 -0300

    evo

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import  express  from 'express';
+import process from 'process';
+import path from 'path';
 
 const host = '0.0.0.0';
 const porta = 3000;
 
 const app = express();
 
-app.get('/', (pega, devolve) => {
-    devolve.write('<h1>Seja bem vindo ao nosso site!</h1>');
-    devolve.end();
-});
+app.use(express.static(path.join(process.cwd(), 'publico')));
 
 app.listen(porta, host, ()=>{
     console.log(`Servidor escutando em http://${host}:${porta}`)
