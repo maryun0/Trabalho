ff91b22f3dd64d65e79adf11e4de92ef34317aa0
Marcelo Nogai <yuassa11@gmail.com>
2024-02-21 15:57:47 -0300

    novo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static(path.join(process.cwd(), 'publico', 'site.html')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'publico', 'site.html'));
+    res.sendFile(path.join(process.cwd(), 'publico', 'jacare.html'));
 });
 
 app.listen(porta, host, ()=>{
